4afbaf7f576dcbf33732416d713f5add5c0c8d2e
stihyary@gmail.com <e166315Ioa>
2023-07-10 20:31:08 +0300

    page_controllers

--- Shop/Shop/urls.py
+++ Shop/Shop/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from mainapp.views import index, catalog, news, contacts, about
+from mainapp.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,4 +9,18 @@ urlpatterns = [
     path('news/', news, name='news'),
     path('contacts/', contacts, name='contacts'),
     path('about/', about, name='about'),
+    path('cart/', cart, name='cart'),
+    path('edit_profile/', edit_profile, name='edit_profile'),
+    path('login/', login, name='login'),
+    path('order_detail/', order_detail, name='order_detail'),
+    path('order_list/', order_list, name='order_list'),
+    path('profile/', profile, name='profile'),
+    path('registration/', registration, name='registration'),
+    path('rules/', rules, name='rules'),
+    path('s_order_detail/', s_order_detail, name='s_order_detail'),
+    path('s_search_detail/', s_search_detail, name='s_search_detail'),
+    path('search_detail/', search_detail, name='search_detail'),
+    path('search_list/', search_list, name='search_list'),
+    path('new_detail/', new_detail, name='new_detail'),
+    path('product_detail/', product_detail, name='product_detail'),
 ]
--- Shop/mainapp/views.py
+++ Shop/mainapp/views.py
@@ -21,3 +21,59 @@ def contacts(request):
 
 def about(request):
     return render(request, 'mainapp/about.html')
+
+
+def cart(request):
+    return render(request, 'mainapp/cart.html')
+
+
+def edit_profile(request):
+    return render(request, 'mainapp/edit_profile.html')
+
+
+def login(request):
+    return render(request, 'mainapp/login.html')
+
+
+def order_detail(request):
+    return render(request, 'mainapp/order_detail.html')
+
+
+def order_list(request):
+    return render(request, 'mainapp/order_list.html')
+
+
+def profile(request):
+    return render(request, 'mainapp/profile.html')
+
+
+def registration(request):
+    return render(request, 'mainapp/registration.html')
+
+
+def rules(request):
+    return render(request, 'mainapp/rules.html')
+
+
+def s_order_detail(request):
+    return render(request, 'mainapp/s_order_detail.html')
+
+
+def s_search_detail(request):
+    return render(request, 'mainapp/s_search_detail.html')
+
+
+def search_detail(request):
+    return render(request, 'mainapp/search_detail.html')
+
+
+def search_list(request):
+    return render(request, 'mainapp/search_list.html')
+
+
+def new_detail(request):
+    return render(request, 'mainapp/single_new.html')
+
+
+def product_detail(request):
+    return render(request, 'mainapp/single_product.html')
