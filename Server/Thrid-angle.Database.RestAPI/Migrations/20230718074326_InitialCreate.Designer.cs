5195d0c94bd963761ba1bd43c36926bcc67e0a74
nick.chuchunov <nick.chuchunov@yandex.ru>
2023-07-18 11:12:47 +0300

    улучшен контекст и написаны методы работы с базой данных

--- Server/Thrid-angle.Database.RestAPI/Migrations/20230718074326_InitialCreate.Designer.cs
+++ Server/Thrid-angle.Database.RestAPI/Migrations/20230718074326_InitialCreate.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Thrid_angle.Database.RestAPI.Database;
 
@@ -10,9 +11,11 @@ using Thrid_angle.Database.RestAPI.Database;
 namespace Thrid_angle.Database.RestAPI.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20230718074326_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.9");
