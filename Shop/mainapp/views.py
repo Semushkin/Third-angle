29ed3ee84014839353cfea3b9d67488c5ce61433
stihyary@gmail.com <e166315Ioa>
2023-07-19 20:41:19 +0300

    pagination

--- Shop/mainapp/views.py
+++ Shop/mainapp/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib import auth
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 
 # Create your views here.
 
@@ -17,10 +18,14 @@ def index(request):
 
 
 def catalog(request):
-    context = {
-        'books': Book.objects.all(),
-    }
+    object_list = Book.objects.all()
+    items_per_page = 1
+    paginator = Paginator(object_list, items_per_page)
+    page_number = request.GET.get('page', 1)
+    page_obj = paginator.get_page(page_number)
+    context = {'page_obj': page_obj}
     return render(request, 'mainapp/catalog.html', context)
+
 
 
 def news(request):
