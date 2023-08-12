c3c3d01c9f20c0f7c1339b11ff82e1d90f47fb60
nick.chuchunov <nick.chuchunov@yandex.ru>
2023-08-12 20:02:39 +0300

    обновление методов REST AP корзины и карточки заказа

---
+++ Server/Thrid-angle.Database.RestAPI/Mehtods/IHelperMethodsDatabase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thrid_angle.Database.RestAPI.Mehtods
+{
+    public interface IHelperMethodsDatabase
+    {
+         public (Guid IdOrderCard, int NumberCard) BasketNewGuidOrderCard(Guid IdUser, bool NewNumberOrderCard);
+    }
+}
--- Server/Thrid-angle.Database.RestAPI/Mehtods/MethodsEntityFrameworcSQLite.cs
+++ Server/Thrid-angle.Database.RestAPI/Mehtods/MethodsEntityFrameworcSQLite.cs
@@ -19,22 +19,22 @@ namespace Thrid_angle.Database.RestAPI.Mehtods
          
      internal void CreateDatabaseBaskets(Baskets table) { db.Add(table); db.SaveChanges(); }
      internal void CreateDatabaseBookCard(BookCard table) { db.Add(table); db.SaveChanges(); }
-     internal void CreateDatabaseOrderCard(OrderCard table) { db.Add(table); db.SaveChanges(); }
+     
      internal void CreateDatabaseQuoteCard(QuoteCard table) { db.Add(table); db.SaveChanges(); }
      internal void CreateDatabaseRequestCard(RequestCard table) { db.Add(table); db.SaveChanges(); }
      internal void CreateDatabaseUserCard(UserCard table)  {db.Add(table); db.SaveChanges();}
 
 
-        internal List<Baskets> ReadDatabaseBaskets() { List<Baskets> t = db.DbBaskets.ToList<Baskets>(); return t;  }
+       
         internal List<BookCard> ReadDatabaseBookCard() { List<BookCard> t = db.DbBookCard.ToList<BookCard>(); return t; }
-        internal List<OrderCard> ReadDatabaseOrderCard() { List<OrderCard> t = db.DbOrderCard.ToList<OrderCard>(); return t; }
+       
         internal List<QuoteCard> ReadDatabaseQuoteCard() { List<QuoteCard> t = db.DbQuoteCard.ToList<QuoteCard>(); return t; }
         internal List<RequestCard> ReadDatabaseRequestCard() { List<RequestCard> t = db.DbRequestCard.ToList<RequestCard>(); return t; }
         internal List<UserCard> ReadDatabaseUserCard() { List<UserCard> t = db.DbUserCard.ToList<UserCard>(); return t; }
 
-        internal Baskets IDReadDatabaseBaskets(Guid Id) { Baskets t = db.DbBaskets.Find(Id); return t; }
+        internal Baskets IDReadDatabaseBaskets(Guid Id) { Baskets t = db.DbBaskets.Find(Id); return t; } // - удалить
         internal BookCard IDReadDatabaseBookCard(Guid Id) { BookCard t = db.DbBookCard.Find(Id); return t; }
-        internal OrderCard IDReadDatabaseOrderCard(Guid Id) { OrderCard t = db.DbOrderCard.Find(Id); return t; }
+       
         internal QuoteCard IDReadDatabaseQuoteCard(Guid Id) { QuoteCard t = db.DbQuoteCard.Find(Id); return t; }
         internal RequestCard IDReadDatabaseRequestCard(Guid Id) { RequestCard t =  db.DbRequestCard.Find(Id); return t; }
         internal UserCard IDReadDatabaseUserCard(Guid Id) { UserCard t = db.DbUserCard.Find(Id); return t; }
@@ -43,19 +43,30 @@ namespace Thrid_angle.Database.RestAPI.Mehtods
 
         internal void UpdateDatabaseBaskets(Baskets baskets) 
         {
-            var _db = db.DbBaskets.Find(baskets.IdBasket);
-            //_db.IdUser = baskets.IdUser;
-           // _db.IdBook = baskets.IdBook;
-            _db.QuantityBooks = baskets.QuantityBooks;
-            _db.PricePerBook = baskets.PricePerBook;
-            //_db.DateCreationBasket = baskets.DateCreationBasket;
-            _db.DateUbdateBasket = baskets.DateUbdateBasket;
+
+
+            db.DbBaskets.Where(d => d.IdBasket == baskets.IdBasket).Where(b=> b.StatusOrderCard== "basket").ExecuteUpdate(b => b.SetProperty(t => t.QuantityBooks, baskets.QuantityBooks));
+            db.DbBaskets.Where(d => d.IdBasket == baskets.IdBasket).Where(b => b.StatusOrderCard == "basket").ExecuteUpdate(b => b.SetProperty(t => t.PricePerBook, baskets.PricePerBook));
+            db.DbBaskets.Where(d => d.IdBasket == baskets.IdBasket).Where(b => b.StatusOrderCard == "basket").ExecuteUpdate(b => b.SetProperty(t => t.DateUbdateBasket, baskets.DateUbdateBasket));
+
             db.SaveChanges();
-        }
 
-
+       }
        
-        internal void UpdateDatabaseBookCard(BookCard bookCard) 
+     
+
+
+
+
+
+
+
+
+    
+
+
+
+    internal void UpdateDatabaseBookCard(BookCard bookCard) 
         {
 
             var _db = db.DbBookCard.Find(bookCard.IdBook);
@@ -74,16 +85,19 @@ namespace Thrid_angle.Database.RestAPI.Mehtods
 
         }
 
-        internal void UpdateDatabaseOrderCard(OrderCard orderCard)
+        internal void UpdateDatabaseOrderCard(Baskets baskets)
         {
-            var _db = db.DbOrderCard.Find(orderCard.IdOrder);
-            _db.OrderCardBooksList = orderCard.OrderCardBooksList;
-           // _db.DateCreationOrderCard = orderCard.DateCreationOrderCard;
-            _db.DateUpdateOrderCard = orderCard.DateUpdateOrderCard;
-            _db.StatusOrderCard = orderCard.StatusOrderCard;
-           // _db.IdUsers = orderCard.IdUsers;
-           db.SaveChanges();
-        
+            db.DbBaskets.Where(d => d.NumberOrderCard == baskets.NumberOrderCard).ExecuteUpdate(b => b.SetProperty(t => t.StatusOrderCard, baskets.StatusOrderCard));
+            db.DbBaskets.Where(d => d.NumberOrderCard == baskets.NumberOrderCard).ExecuteUpdate(b => b.SetProperty(t => t.DateUbdateBasket, baskets.DateUbdateBasket));
+
+
+           
+
+            db.SaveChanges();
+
+
+          
+
         }
 
 
@@ -137,9 +151,9 @@ namespace Thrid_angle.Database.RestAPI.Mehtods
 
         }
 
-        internal void DeleteDatabaseBaskets(Guid Id) { db.DbBaskets.Where(d => d.IdBasket == Id).ExecuteDelete(); db.SaveChanges(); }
+        internal void DeleteDatabaseBaskets(Guid Id) { db.DbBaskets.Where(d => d.IdBasket == Id&d.StatusOrderCard== "basket").ExecuteDelete(); db.SaveChanges(); }
         internal void DeleteDatabaseBookCard(Guid Id) { db.DbBookCard.Where(d => d.IdBook == Id).ExecuteDelete(); db.SaveChanges(); }
-        internal void DeleteDatabaseOrderCard(Guid Id) { db.DbOrderCard.Where(d => d.IdOrder == Id).ExecuteDelete(); db.SaveChanges(); }
+        internal void DeleteDatabaseOrderCard(Guid Id) { db.DbBaskets.Where(d => d.NumberOrderCard == Id).ExecuteDelete(); db.SaveChanges(); }
         internal void DeleteDatabaseQuoteCard(Guid Id) { db.DbQuoteCard.Where(d => d.IdQuote == Id).ExecuteDelete(); db.SaveChanges(); }
         internal void DeleteDatabaseRequestCard(Guid Id) { db.DbRequestCard.Where(d => d.IdRequestCard == Id).ExecuteDelete(); db.SaveChanges(); }
         internal void DeleteDatabaseUserCard(Guid Id) { db.DbUserCard.Where(d => d.IdUser == Id).ExecuteDelete(); db.SaveChanges(); }
@@ -147,10 +161,10 @@ namespace Thrid_angle.Database.RestAPI.Mehtods
 
 
 
-        internal List<Baskets> UserReadDatabaseBaskets(Guid IdUser) { List<Baskets> t = db.DbBaskets.Where(d => d.IdUser == IdUser).ToList<Baskets>(); return t; }
+        internal List<Baskets> UserReadDatabaseBaskets(Guid IdUser) { List<Baskets> t = db.DbBaskets.Where(d => d.IdUser == IdUser && d.StatusOrderCard == "basket").ToList<Baskets>(); return t; }
 
 
-        internal List<OrderCard> UserReadDatabaseOrderCard(Guid IdUser) { List<OrderCard> t = db.DbOrderCard.Where(d => d.IdUsers == IdUser).ToList<OrderCard>(); return t; }
+        internal List<Baskets> UserReadDatabaseOrderCard(Guid IdUser) { List<Baskets> t = db.DbBaskets.Where(d =>  d.IdUser == IdUser&&d.StatusOrderCard== "order").ToList<Baskets>(); return t; }
 
 
         internal List<RequestCard> IdUserReadDatabaseRequestCard(Guid IdUser) { List<RequestCard> t = db.DbRequestCard.Where(d => d.IdUser == IdUser).ToList<RequestCard>(); return t; }
@@ -160,6 +174,21 @@ namespace Thrid_angle.Database.RestAPI.Mehtods
 
 
         internal List<UserCard> LoginUserReadDatabaseUserCard(string LoginUser, string PasswordUser) { List<UserCard> t = db.DbUserCard.Where(d => d.LoginUser == LoginUser&&d.PasswordUser== PasswordUser).ToList<UserCard>(); return t; }
+
+
+        internal void CreateDatabaseBasketsStatusOrderCard(Guid IdUser)
+        {
+           
+                var t = db.DbBaskets.Where(d => d.StatusOrderCard == "basket"& d.IdUser== IdUser);
+
+                t.ExecuteUpdate(setters => setters.SetProperty(b => b.StatusOrderCard, "order"));
+
+                db.SaveChanges();
+
+            
+
+
+        }
 
 
 
